5ad63b5ca6318411171a4a2d0c5b0a096bf33090
Yargachin-wye <78979106+Yargachin-wye@users.noreply.github.com>
2023-05-17 11:34:14 +0700

    first

---
+++ P2P_chat/Assets/Client.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+
+public class Client
+{
+    private static string _name;
+    private static UdpClient _client;
+    private static IPEndPoint _remoteEndPoint;
+    private static IPEndPoint _localEndPoint;
+    public static void StartClient(string name, string ipAddressString, int port)
+    {
+        _name = name;
+        IPAddress remoteAddress = IPAddress.Parse(ipAddressString);
+        Debug.Log("["+ remoteAddress + ":"+port+"]");
+
+        _remoteEndPoint = new IPEndPoint(remoteAddress, port);
+        _localEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+        ThreadStart threadStart = new ThreadStart(Main);
+        Thread thread = new Thread(threadStart);
+        thread.Start();
+    }
+    static void Main()
+    {
+        
+        _client = new UdpClient();
+        _client.Client.Bind(_localEndPoint);
+
+        Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+        receiveThread.Start();
+    }
+
+    public static void SendMessage(string message)
+    {
+        message = _name + ": " + message;
+        byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+        _client.Send(data, data.Length, _remoteEndPoint);
+    }
+
+    private static void ReceiveMessage()
+    {
+        while (true)
+        {
+            byte[] data = _client.Receive(ref _localEndPoint);
+            string message = System.Text.Encoding.ASCII.GetString(data);
+            MainManeger.ShowMessage(message);
+        }
+    }
+}
---
+++ P2P_chat/Assets/MainManeger.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
+
+public class MainManeger : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _ipText;
+    [SerializeField] private TextMeshProUGUI _inMessageText;
+    private static TextMeshProUGUI _message;
+    [SerializeField] private TMP_InputField _ipInputField;
+    [SerializeField] private TMP_InputField _portInputField;
+    [SerializeField] private TMP_InputField _outMessageText;
+    [SerializeField] private GameObject SendMessageButton;
+
+    private void Start()
+    {
+        _message = _inMessageText;
+        string ipAddressString = GetLocalIPAddress();
+        int port = GetAvailablePort();
+
+        _ipText.text = "my ip:\n" + ipAddressString + "\nfree port:\n" + port;
+        SendMessageButton.SetActive(false);
+    }
+    public void DoSomeDirtyShit()
+    {
+        Client.StartClient("jopicson", _ipInputField.text, int.Parse(_portInputField.text));
+
+        SendMessageButton.SetActive(true);
+    }
+    public void SendMessage()
+    {
+        Client.SendMessage(_outMessageText.text);
+    }
+    public static void ShowMessage(string str)
+    {
+        _message.text = str + _message.text;
+    }
+    private string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        Debug.LogWarning("IP-адрес не найден.");
+        return " ";
+    }
+    private int GetAvailablePort()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Loopback, 0);
+        listener.Start();
+        int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+        listener.Stop();
+        return port;
+    }
+}
