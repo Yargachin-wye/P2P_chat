c26e8fb9052e0703b2eadee133885079be9e8f12
Yargachin-wye <78979106+Yargachin-wye@users.noreply.github.com>
2023-05-17 14:25:36 +0700

    p2p_Tests_v6

--- P2P_chat/Assets/Client.cs
+++ P2P_chat/Assets/Client.cs
@@ -9,29 +9,36 @@ public class Client
 {
     private static string _name;
     private static IPEndPoint _remoteEndPoint;
+    private static IPEndPoint _endPoint;
     private static int _port;
 
     private static UdpClient _client;
     public static void StartClient(string name, string ipAddressString, int port)
     {
         _name = name;
-        IPAddress remoteAddress = IPAddress.Parse(ipAddressString);
+        IPAddress ipAddress = IPAddress.Parse(ipAddressString);
 
-        _remoteEndPoint = new IPEndPoint(remoteAddress, port);
-
+        _remoteEndPoint = new IPEndPoint(ipAddress, port);
+        _endPoint = new IPEndPoint(ipAddress, port);
         _port = port;
 
-        Debug.Log("[" + remoteAddress + ":" + _port + "]");
+        Debug.Log("[" + ipAddress + ":" + _port + "]");
 
         ThreadStart threadStart = new ThreadStart(ReceiveMessage);
         Thread thread = new Thread(threadStart);
         thread.Start();
     }
-    public static void SendMessage(string message)
+    public static void SendMessage(string str)
     {
-        message = _name + ": " + message;
-        byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
-        _client.Send(data, data.Length, _remoteEndPoint);
+        string message = _name + ": " + str;
+        UdpClient client = new UdpClient();
+
+        // Отправка сообщения
+        byte[] bytes = System.Text.Encoding.ASCII.GetBytes(message);
+        client.Send(bytes, bytes.Length, _endPoint);
+
+        // Закрытие UDP клиента
+        client.Close();
     }
 
     private static void ReceiveMessage()
@@ -43,6 +50,7 @@ public class Client
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
             byte[] receivedBytes = client.Receive(ref remoteEndPoint);
             string receivedMessage = System.Text.Encoding.ASCII.GetString(receivedBytes);
+            MainManeger.ShowMessage(receivedMessage);
         }
         client.Close();
     }
